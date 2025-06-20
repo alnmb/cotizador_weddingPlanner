4c940ae6c8a9a4a0b88e2f5e7338e63ce2cc4399
Alan Martinez <alnfnx@gmail.com>
2025-06-20 15:45:20 -0600

    se corrige la linea del app.run

--- app.py
+++ app.py
@@ -290,5 +290,7 @@ def pagos():
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
